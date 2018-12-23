d231fe3c853bd447a5e83c5903fe7b73f1481459
Kate <ke.sanders4404@gmail.com>
2018-12-23 11:33:26 -0700

    make the event handler accept an argument
    Changed the handleIncrement method to accept a parameter.

--- components/counter.jsx
+++ components/counter.jsx
@@ -10,7 +10,7 @@ class Counter extends Component {
   //     this.handleIncrement = this.handleIncrement.bind(this);
   // }
 
-  handleIncrement = () => {
+  handleIncrement = product => {
     this.setState({ count: this.state.count + 1 });
   };
 
@@ -19,7 +19,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement(product)}
           className="btn btn-secondary btn-sm"
         >
           Increment

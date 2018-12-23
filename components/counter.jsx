703d03d4679403a98614373c0ad7edf0c4bf7bfc
Kate <ke.sanders4404@gmail.com>
2018-12-23 11:46:12 -0700

    fixed error
    Changed product to a specific object so that there is not an error from product not existing

--- components/counter.jsx
+++ components/counter.jsx
@@ -19,7 +19,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement(product)}
+          onClick={() => this.handleIncrement({ id: 1 })}
           className="btn btn-secondary btn-sm"
         >
           Increment

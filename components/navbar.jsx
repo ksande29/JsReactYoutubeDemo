ab630def7830c4129cb1a2a082d0996a46795893
Kate <ke.sanders4404@gmail.com>
2018-12-27 15:08:39 -0700

    Added a total on the navbar
    Created a total of how many counters have a value > 0 which is displayed on the navbar.

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -6,6 +6,9 @@ class NavBar extends Component {
       <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand" href="#">
           Navbar
+          <span className="badge badge-pill badge-secondary m-1">
+            {this.props.totalCounters}
+          </span>
         </a>
       </nav>
     );

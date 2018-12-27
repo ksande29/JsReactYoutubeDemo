b56dc3348e5bb7d99552a2fa60a79562a13c625b
Kate <ke.sanders4404@gmail.com>
2018-12-27 11:11:20 -0700

    Passed the title as a child prop
    Learned about the children property and used it to pass the title of each counter to the page

--- components/counter.jsx
+++ components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: this.props.value
+    value: this.props.value
   };
 
   // constructor() {
@@ -11,14 +11,15 @@ class Counter extends Component {
   // }
 
   handleIncrement = product => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
-    console.log("props", this.props);
+    console.log(this.props);
 
     return (
       <div>
+        {this.props.children};
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => this.handleIncrement({ id: 1 })}
@@ -32,12 +33,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    return this.state.count === 0 ? "Zero" : this.state.count;
+    return this.state.value === 0 ? "Zero" : this.state.value;
   }
 }
 

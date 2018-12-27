282b8ea1c9ef274940b39e378a82b5a6b767b93a
Kate <ke.sanders4404@gmail.com>
2018-12-27 15:15:04 -0700

    possiblity for stateless functional component
    Added a comment above the class navbar showing how navbar could be made with a stateless functional component instead of a class

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,5 +1,19 @@
 import React, { Component } from "react";
 
+//anoher option instead of using a class
+//stateless functional component
+/*const NavBar = (props) => {
+    return (
+        <nav className="navbar navbar-light bg-light">
+          <a className="navbar-brand" href="#">
+            Navbar
+            <span className="badge badge-pill badge-secondary m-1">
+              {props.totalCounters}
+            </span>
+          </a>
+        </nav>
+        );
+}*/
 class NavBar extends Component {
   render() {
     return (
